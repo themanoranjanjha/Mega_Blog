d8c9938ca6a0ecf7a1cd12f9a66b0e1a0cb1458b
Manoranjan1k <manasj790@gmail.com>
2024-02-13 22:58:27 +0530

    auth updated

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf.js";
+import {client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new client();
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);    
+
+    }
+    async createAccount({email, password, Name}){
+        // eslint-disable-next-line no-useless-catch
+        try {
+           const userAccount = await this.account.create(ID.unique(), email, password, Name);
+           if(userAccount){
+            return this.login({email, password});
+        }else{
+            return userAccount;
+        }
+        }catch (error) {
+            throw error;
+     }
+
+  }
+    async login({email, password}){
+        // eslint-disable-next-line no-useless-catch
+        try {
+             await this.account.createEmailSession(email, password);
+            }
+         catch (error) {
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        // eslint-disable-next-line no-useless-catch
+        try {   
+            return await this.account.get();
+            
+        }
+        catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error",error);
+        }
+        // eslint-disable-next-line no-unreachable
+        return null;
+    }
+
+    async logout(){
+        // eslint-disable-next-line no-useless-catch
+        try {
+            await this.account.deleteSessions();
+        }
+        catch (error) {
+            console.log("Appwrite serive :: logout :: error",error);
+        }
+    }
+}
+const authService = new AuthService();
+
+export default authService;

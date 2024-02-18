2a3ab25ad35b1793479e007661754d3503d07c04
Manoranjan1k <manasj790@gmail.com>
2024-02-18 23:35:14 +0530

    auth layot done

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+import { UseSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+
+export default function Protected({children, authentication= true}) {
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = UseSelector(state => state.auth.status)
+
+    useEffect(() => {
+       if (authentication && authStatus !== authentication) {
+             navigate("/login")
+       }else if (!authentication && authStatus !== authentication) {
+           navigate("/")
+       }
+        setLoader(false)
+    }[authStatus, navigate, authentication])
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+}
+

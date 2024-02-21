60664eda98f7b49ff6055e3e1bdf090e03936528
Manoranjan1k <manasj790@gmail.com>
2024-02-21 22:49:58 +0530

    all pages Updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import {Container, Logo, LogoutBtn} from '../index'
 import {Link} from 'react-router-dom'
-import { UseSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 function Header() {
-  const authStatus = UseSelector(state => state.auth.status)
+  const authStatus = useSelector(state => state.auth.status)
   const navigate = useNavigate()
 
   const navItems = [
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,33 @@
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react"
+import { Container, PostForm } from "../components"
+import appwriteService from "../appwrite/config"
+
+function EditPost() {
+    const [post, setPost] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(slug){
+            appwriteService.getPost(slug).then((post)=> {
+                if (post) {
+                    setPost(post)
+                }
+            })
+        }else{
+            navigate('/')
+        }
+        
+    },[slug, navigate])
+ 
+  return post? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ):null
+}
+
+export default EditPost
